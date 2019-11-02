5d9a18491f6ff0b9cf5e5e768ea19b2831bd89cf
Changhyeon <pch313@kaist.ac.kr>
2019-11-02 19:56:43 +0900

    Initial Commit

---
+++ StrongGuy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StrongGuy
+//
+//  Created by 박창현 on 11/2/19.
+//  Copyright © 2019 박창현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
